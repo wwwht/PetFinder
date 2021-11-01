d1f884c0357b36f7a185c46bd1b8dfa7e927d5f2
wwwht <wht_no1@outlook.com>
2021-11-01 21:45:38 +0800

    add model

--- src/models/pet_model.py
+++ src/models/pet_model.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-10-31 16:24:15
-LastEditTime: 2021-10-31 16:30:36
+LastEditTime: 2021-11-01 21:45:20
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \PetFinder\src\models\pet_model.py
@@ -11,6 +11,12 @@ import torch
 from pytorch_lightning import LightningModule
 import numpy as np
 import pandas as pd
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from timm import create_model
+from torchmetrics.classification.accuracy import Accuracy
+from src.models.modules.simple_dense_net import SimpleDenseNet
 
 def mixup(x: torch.Tensor, y:torch.Tensor, alpha:float = 1.0):
     assert alpha > 0, "alpha should be larger than 0"
@@ -23,7 +29,102 @@ def mixup(x: torch.Tensor, y:torch.Tensor, alpha:float = 1.0):
     return mixed_x, target_a, target_b, lam
 
 class PetModel(LightningModule):
-    def __init__(self, cfg):
+    def __init__(
+        self,
+        cfg
+    ):
         super().__init__()
+
+        # this line ensures params passed to LightningModule will be saved to ckpt
+        # it also allows to access params with 'self.hparams' attribute
         self.cfg = cfg
-        self.__build
+        self.__build_model()
+        self._criterion = eval(self.cfg.loss)()
+        self.save_hyperparameters()
+
+    def __build_model(self):
+        self.backbone = create_model(
+            self.cfg.model.name, pretrained=True, num_classes=0, in_chans=3
+        )
+        num_features = self.backbone.num_features
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5), nn.Linear(num_features, self.cfg.model.output_dim)
+        )
+
+    def forward(self, x: torch.Tensor):
+        # return self.model(x)
+        f = self.backbone(x)
+        out = self.fc(f)
+        return out
+
+    def step(self, batch: Any, mode: str):
+        images, labels = batch
+        labels = labels.float() / 100.0
+        images = self.transform[mode](images)
+        
+        if torch.rand(1)[0] < 0.5 and mode == 'train':
+            mix_images, target_a, target_b, lam = mixup(images, labels, alpha=0.5)
+            logits = self.forward(mix_images).squeeze(1)
+            loss = self._criterion(logits, target_a) * lam + \
+                (1 - lam) * self._criterion(logits, target_b)
+        else:
+            logits = self.forward(images).squeeze(1)
+            loss = self._criterion(logits, labels)
+        
+        pred = logits.sigmoid().detach().cpu() * 100.
+        labels = labels.detach().cpu() * 100.
+        return loss, pred, labels
+
+    def training_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+
+        # log train metrics
+        acc = self.train_accuracy(preds, targets)
+        self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("train/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+
+        # we can return here dict with any tensors
+        # and then read it in some callback or in training_epoch_end() below
+        # remember to always return loss from training_step, or else backpropagation will fail!
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def training_epoch_end(self, outputs: List[Any]):
+        # `outputs` is a list of dicts returned from `training_step()`
+        pass
+
+    def validation_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+
+        # log val metrics
+        acc = self.val_accuracy(preds, targets)
+        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def validation_epoch_end(self, outputs: List[Any]):
+        pass
+
+    def test_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.step(batch)
+
+        # log test metrics
+        acc = self.test_accuracy(preds, targets)
+        self.log("test/loss", loss, on_step=False, on_epoch=True)
+        self.log("test/acc", acc, on_step=False, on_epoch=True)
+
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def test_epoch_end(self, outputs: List[Any]):
+        pass
+
+    def configure_optimizers(self):
+        """Choose what optimizers and learning-rate schedulers to use in your optimization.
+        Normally you'd need one. But in the case of GANs or similar you might have multiple.
+
+        See examples here:
+            https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers
+        """
+        return torch.optim.Adam(
+            params=self.parameters(), lr=self.hparams.lr, weight_decay=self.hparams.weight_decay
+        )
